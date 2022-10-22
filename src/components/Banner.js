4fba3a6c6b4527c3e658066b00ea6a3e1238fb9b
Akash Shakya <akashshakya752@gmail.com>
2022-10-22 14:54:10 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,11 +3,14 @@ import Money from '../assets/money-management.svg'
 import SearchIcon from '../assets/icon_search_.svg'
 
 export default function Banner(props) {
+ 
+
+
   return (
     <div className='banner'>
       <p className='referral-heading'>Referral Status<span><img src={Money} alt='money' style={{marginLeft:"10px"}}></img></span> </p> 
       <div >
-        <input className='search-box' type="text"  placeholder="Search" />
+        <input className='search-box' type="text"  placeholder="Search" onChange={props.searchHandle} value={props.text}/>
         <span ><img className='search-icon' src={SearchIcon} alt='money'></img></span>
       </div>
     </div>
